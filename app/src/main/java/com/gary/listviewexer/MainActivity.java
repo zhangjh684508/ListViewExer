376e755c217203f5c76d30a6000336ac9a17f6bc
zhangjh684508 <583887669@qq.com>
2016-07-24 15:35:32 +0800

    优化view

--- app/src/main/java/com/gary/listviewexer/MainActivity.java
+++ app/src/main/java/com/gary/listviewexer/MainActivity.java
@@ -79,12 +79,36 @@ public class MainActivity extends AppCompatActivity {
         public View getView(int position, View convertView, ViewGroup parent)
         {
             Fruit fruit=getItem(position);
-            View view= LayoutInflater.from(getContext()).inflate(resourceId,null);
-            ImageView fruitImage=(ImageView)view.findViewById(R.id.fruit_image);
-            TextView fruitName=(TextView)view.findViewById(R.id.fruit_name);
-            fruitImage.setImageResource(fruit.getImageId());
-            fruitName.setText(fruit.getmName());
+
+            View view;
+            ViewHolder viewHolder;
+
+            if (convertView==null)
+            {
+                view= LayoutInflater.from(getContext()).inflate(resourceId,null);
+                viewHolder.fruitImage=(ImageView)view.findViewById(R.id.fruit_image);
+                viewHolder.fruitName=(TextView)view.findViewById(R.id.fruit_name);
+                view.setTag(viewHolder);
+            }
+            else
+            {
+                view =convertView;
+                viewHolder=(ViewHolder)view.getTag();
+            }
+
+            //View view= LayoutInflater.from(getContext()).inflate(resourceId,null);
+            //ImageView fruitImage=(ImageView)view.findViewById(R.id.fruit_image);
+            //TextView fruitName=(TextView)view.findViewById(R.id.fruit_name);
+
+            viewHolder.fruitImage.setImageResource(fruit.getImageId());
+            viewHolder.fruitName.setText(fruit.getmName());
             return view;
+        }
+
+        class ViewHolder
+        {
+            ImageView fruitImage;
+            TextView fruitName;
         }
 
     }
